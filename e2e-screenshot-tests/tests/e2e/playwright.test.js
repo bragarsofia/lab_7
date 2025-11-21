e80a9516048cfd6d53959b95bd74e1135dd17209
bragarsofia <162445166+bragarsofia@users.noreply.github.com>
2025-11-22 01:18:22 +0200

    Update playwright.test.js

--- e2e-screenshot-tests/tests/e2e/playwright.test.js
+++ e2e-screenshot-tests/tests/e2e/playwright.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Перевірка заголовка сайту', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Ламаю тест/);
 });
 
 test('Перевірка наявності меню навігації', async ({ page }) => {
